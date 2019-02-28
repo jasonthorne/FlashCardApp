ac9c7632799948dfa5daad59c78a4b00ba1c1f1f
jasonthorne <jasonthorne@live.ie>
2019-02-28 19:03:58 +0000

    16. showNextCard()

--- src/com/jay/FlashcardApp/FlashcardPlayer.java
+++ src/com/jay/FlashcardApp/FlashcardPlayer.java
@@ -21,7 +21,7 @@ public class FlashcardPlayer {
     private Flashcard currentCard;
     private int currentCardIndex;
     private JFrame frame;
-    private JButton showAnwserButton;
+    private JButton myButton;
     private boolean isShowAnswer;
 
 
@@ -49,9 +49,9 @@ public class FlashcardPlayer {
         questionScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS); //ensure scroll bar is always vertical
         questionScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER); //ensure NO horizonal scrollbar
 
-        //create next button
-        showAnwserButton = new JButton("Show answer");
-        showAnwserButton.addActionListener(new NextCardListener()); //create and add a 'NextCardListener' obj to button's actionListener
+        //create 'next card'/'show next' button
+        myButton = new JButton("Show answer");
+        myButton.addActionListener(new NextCardListener()); //create and add a 'NextCardListener' obj to button's actionListener
 
         //Create a menu
         JMenuBar menuBar = new JMenuBar();
@@ -72,7 +72,7 @@ public class FlashcardPlayer {
 
         //add components to main panel (ORDER MATTERS!)
         mainPanel.add(questionScrollPane);
-        mainPanel.add(showAnwserButton);
+        mainPanel.add(myButton);
 
         //add mainPanel to frame
         frame.getContentPane().add(BorderLayout.CENTER, mainPanel); //get the content pane from the frame, and add components to that. (one which centers everything, and 'mainPanel')
@@ -95,9 +95,28 @@ public class FlashcardPlayer {
 
     }
 
+    //triggers when button
     private class NextCardListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
+
+            if(isShowAnswer){
+                display.setText(currentCard.getAnswer());
+                myButton.setText("Next card"); //change text on button to "Next card, as they've already seen the answer"
+                isShowAnswer = false; //reset the flag
+            }else{
+
+                //show next question
+                if (cardIterator.hasNext()){//if iterator still has more cards in it:
+
+                    showNextCard();
+                }else{ //card iterator is empty
+
+                    display.setText("That was the last card.");
+                    myButton.setEnabled(false); //disable the button to prevent error
+                }
+
+            }
 
         }
     }
@@ -133,19 +152,17 @@ public class FlashcardPlayer {
                 makeCard(line); //send line contents to makeCard()
             }
 
-
-
         }catch (Exception e){
 
             System.out.println("Oh noes! Couldn't read file!");
             e.printStackTrace();
-
         }
 
         //show the first card in the list:
-        cardIterator = cardList.iterator();
-
+        cardIterator = cardList.iterator(); //create the iterator for the cardList array
+        showNextCard();
     }
+
 
     private void makeCard(String lineToParse) {
 
@@ -171,4 +188,15 @@ public class FlashcardPlayer {
         }
 
     }
+
+    private void showNextCard() {
+        currentCard = (Flashcard) cardIterator.next(); //cast to 'Flashcard' for safety! (though shouldnt be needed!)
+
+        display.setText(currentCard.getQuestion());
+        myButton.setText("Show Answer"); //?????????????????????????????????
+        isShowAnswer = true;
+    }
+
+
+
 }
